45d4da8960d9b82c2ef55982eeafd80b66eaf1c0
koidzt <koi27792@gmail.com>
2021-03-02 11:42:12 +0700

    REDTful API pattern .then

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    axios
+      .all([
+        axios.post('https://api.dev.farmbook.co/mbapi/test', { fn: 'nodeJS' }),
+        axios.post('https://api.dev.farmbook.co/mbapi/test', { fn: 'nodeJS' }),
+        axios.post('https://api.dev.farmbook.co/mbapi/test', { fn: 'nodeJS' }),
+        axios.post('https://api.dev.farmbook.co/mbapi/test', { fn: 'nodeJS' }),
+        axios.post('https://api.dev.farmbook.co/mbapi/test', { fn: 'nodeJS' }),
+      ])
+      .then(
+        axios.spread((resp1, resp2, resp3, resp4, resp5) => {
+          console.log(
+            resp1.data.data +
+              '\n' +
+              resp2.data.data +
+              '\n' +
+              resp3.data.data +
+              '\n' +
+              resp4.data.data +
+              '\n' +
+              resp5.data.data
+          );
+        })
+      )
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>Test Project</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1>Test</h1>
+      </header>
     </div>
   );
 }
