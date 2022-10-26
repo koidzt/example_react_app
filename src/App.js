bfa4fd96e8b8f6680c72470a9c8184a494deac86
koidzt <koi27792@gmail.com>
2022-10-26 17:32:39 +0700

    Second React - Counter React (Management state)

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Person from './components/PersonCard';
 
-class App extends Component {
-  // new version react for setState by class
-  // remove constructor(props){super()} remove this. before render() but function need arrow function
+class App extends React.Component {
   state = {
-    isWhite: true,
-    bgColor: '',
-    inputValue: '',
+    // inputValue: "",
+    count: 0,
   };
-
-  changeBgColor = () => {
-    this.setState({ bgColor: this.state.inputValue });
-  };
-
-  changeIsWhiteToFalse = () => {
-    this.setState({ isWhite: false });
-  };
-
-  changeInputValue = (e) => {
-    this.setState({ inputValue: e.target.value });
-  };
-
   render() {
-    //this.state.isWhite เรียกใช้ได้ แต่ห้ามทำแบบบรรทัดล่างเด็จขาด
-    //this.state.isWhite = false; แต่ห้ามกำหนดค่าตรงๆที่จุด render
-    //ถ้ากำหนดค่าให้ใช้ this.setState.isWhite({ isWhite: false })
-    let background = this.state.isWhite ? 'white' : this.state.bgColor ? this.state.bgColor : 'lightcyan';
     return (
-      <div className="App" style={{ backgroundColor: background }}>
-        <input value={this.state.inputValue} onChange={this.changeInputValue} />
-        <button onClick={this.changeBgColor}>Change to input color</button>
-        <button onClick={this.changeIsWhiteToFalse}>Change is not white background color.</button>
-        <Person name="Koi" age={28} color="Black" />
-        <Person name="Cake" age={29} color="Blue" />
-        <Person name="Pup" age={27} color="Green" />
+      <div className="App" style={{ margin: '1em' }}>
+        {/* <input
+          onChange={(e) => this.setState({ inputValue: e.target.value })}
+          style={{ width: "400px", height: "50px", fontSize: "40px" }} />
+        <h1>{this.state.inputValue}</h1>   */}
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+          style={{ fontSize: '4em', width: '3em', padding: '0.2em' }}
+        >
+          UP
+        </button>
+        <h1 style={{ fontSize: '10em' }}>{this.state.count}</h1>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count - 1 });
+          }}
+          disabled={this.state.count === 0}
+          style={{ fontSize: '4em', padding: '0.2em' }}
+        >
+          DOWN
+        </button>
+        <button
+          onClick={() => this.setState({ count: 0 })}
+          style={{ fontSize: '4em', padding: '0.2em', margin: '0.2em' }}
+        >
+          RESET
+        </button>
       </div>
     );
   }
