d1e24cbfb51681c9dbac63818663399f8ecf3996
koidzt <koi27792@gmail.com>
2022-10-26 15:19:23 +0700

    class component is new version react by class component parttern

--- src/App.js
+++ src/App.js
@@ -4,33 +4,34 @@ import './App.css';
 import Person from './components/PersonCard';
 
 class App extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      isWhite: true,
-      bgColor: '',
-      inputValue: '',
-    };
-    this.changeIsWhiteToFalse = this.changeIsWhiteToFalse.bind(this);
-    this.changeBgColor = this.changeBgColor.bind(this);
-  }
+  // new version react for setState by class
+  // remove constructor(props){super()} remove this. before render() but function need arrow function
+  state = {
+    isWhite: true,
+    bgColor: '',
+    inputValue: '',
+  };
 
-  changeBgColor() {
+  changeBgColor = () => {
     this.setState({ bgColor: this.state.inputValue });
-  }
+  };
 
-  changeIsWhiteToFalse() {
+  changeIsWhiteToFalse = () => {
     this.setState({ isWhite: false });
-  }
+  };
+
+  changeInputValue = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
 
   render() {
     //this.state.isWhite เรียกใช้ได้ แต่ห้ามทำแบบบรรทัดล่างเด็จขาด
     //this.state.isWhite = false; แต่ห้ามกำหนดค่าตรงๆที่จุด render
-    //ถ้ากำหนดค่าให้ใช้ this.setState.isWhite(false) หรือ this.setState.isWhite({ isWhite: false })
+    //ถ้ากำหนดค่าให้ใช้ this.setState.isWhite({ isWhite: false })
     let background = this.state.isWhite ? 'white' : this.state.bgColor ? this.state.bgColor : 'lightcyan';
     return (
       <div className="App" style={{ backgroundColor: background }}>
-        <input value={this.state.inputValue} onChange={(e) => this.setState({ inputValue: e.target.value })} />
+        <input value={this.state.inputValue} onChange={this.changeInputValue} />
         <button onClick={this.changeBgColor}>Change to input color</button>
         <button onClick={this.changeIsWhiteToFalse}>Change is not white background color.</button>
         <Person name="Koi" age={28} color="Black" />
