2234d5b75c6ca9899fa5ab9f60a24c63f2b26502
koidzt <koi27792@gmail.com>
2023-03-30 22:39:42 +0700

    do exercise 5-6 of 10 Exercises react  tranform  to react hook

--- src/components/contactForm/index.js
+++ src/components/contactForm/index.js
@@ -1,66 +1,139 @@
 import React, { useState } from 'react';
 import './index.css';
 
+// function ContactForm() {
+//   const [firstname, setFirstname] = useState('');
+//   const [lastname, setLastname] = useState('');
+//   const [complain, setComplain] = useState('');
+//   const [ratings, setRatings] = useState('');
+
+//   function handleFirstnameChange(e) {
+//     setFirstname(e.target.value);
+//   }
+
+//   function handleLastnameChange(e) {
+//     setLastname(e.target.value);
+//   }
+
+//   function handleComplainChange(e) {
+//     setComplain(e.target.value);
+//   }
+
+//   function handleRatingChange(e) {
+//     setRatings(e.target.value);
+//   }
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     alert(
+//       JSON.stringify({
+//         firstname,
+//         lastname,
+//         ratings,
+//         complain,
+//       })
+//     );
+//     resetForm();
+//   }
+
+//   function resetForm() {
+//     setFirstname('');
+//     setLastname('');
+//     setRatings('');
+//     setComplain('');
+//   }
+
+//   return (
+//     <form className="form-container" onSubmit={handleSubmit}>
+//       <div className="field-container">
+//         <label htmlFor="firstname">First Name</label>
+//         <input id="firstname" name="firstname" type="text" value={firstname} onChange={handleFirstnameChange} />
+//       </div>
+//       <div className="field-container">
+//         <label htmlFor="lastname">Last Name</label>
+//         <input id="lastname" name="lastname" type="text" value={lastname} onChange={handleLastnameChange} />
+//       </div>
+//       <div className="field-container">
+//         <label htmlFor="complain">Complain</label>
+//         <textarea id="complain" name="complain" type="text" rows={5} value={complain} onChange={handleComplainChange} />
+//       </div>
+
+//       <div className="field-container">
+//         <label htmlFor="ratings">Ratings</label>
+//         <select name="ratings" id="ratings" value={ratings} onChange={handleRatingChange}>
+//           <option value="">Select Ratings</option>
+//           <option value="1">1 Star</option>
+//           <option value="2">2 Star</option>
+//           <option value="3">3 Star</option>
+//           <option value="4">4 Star</option>
+//           <option value="5">5 Star</option>
+//         </select>
+//       </div>
+
+//       <button className="button button-submit" type="submit">
+//         Submit
+//       </button>
+//     </form>
+//   );
+// }
+
 function ContactForm() {
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
-  const [complain, setComplain] = useState('');
-  const [ratings, setRatings] = useState('');
+  const initialValueForm = {
+    firstname: '',
+    lastname: '',
+    complain: '',
+    ratings: '',
+  };
 
-  function handleFirstnameChange(e) {
-    setFirstname(e.target.value);
+  const [valueForm, setValueForm] = useState(initialValueForm);
+
+  function handleChangeValue(key) {
+    return (event) => setValueForm({ ...valueForm, [key]: event.target.value });
   }
 
-  function handleLastnameChange(e) {
-    setLastname(e.target.value);
-  }
-
-  function handleComplainChange(e) {
-    setComplain(e.target.value);
-  }
-
-  function handleRatingChange(e) {
-    setRatings(e.target.value);
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    alert(
-      JSON.stringify({
-        firstname,
-        lastname,
-        ratings,
-        complain,
-      })
-    );
-    resetForm();
-  }
-
-  function resetForm() {
-    setFirstname('');
-    setLastname('');
-    setRatings('');
-    setComplain('');
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(JSON.stringify(valueForm));
+    setValueForm(initialValueForm);
   }
 
   return (
     <form className="form-container" onSubmit={handleSubmit}>
       <div className="field-container">
         <label htmlFor="firstname">First Name</label>
-        <input id="firstname" name="firstname" type="text" value={firstname} onChange={handleFirstnameChange} />
+        <input
+          id="firstname"
+          name="firstname"
+          type="text"
+          value={valueForm.firstname}
+          onChange={handleChangeValue('firstname')}
+        />
       </div>
       <div className="field-container">
         <label htmlFor="lastname">Last Name</label>
-        <input id="lastname" name="lastname" type="text" value={lastname} onChange={handleLastnameChange} />
+        <input
+          id="lastname"
+          name="lastname"
+          type="text"
+          value={valueForm.lastname}
+          onChange={handleChangeValue('lastname')}
+        />
       </div>
       <div className="field-container">
         <label htmlFor="complain">Complain</label>
-        <textarea id="complain" name="complain" type="text" rows={5} value={complain} onChange={handleComplainChange} />
+        <textarea
+          id="complain"
+          name="complain"
+          type="text"
+          rows={5}
+          value={valueForm.complain}
+          onChange={handleChangeValue('complain')}
+        />
       </div>
 
       <div className="field-container">
         <label htmlFor="ratings">Ratings</label>
-        <select name="ratings" id="ratings" value={ratings} onChange={handleRatingChange}>
+        <select name="ratings" id="ratings" value={valueForm.ratings} onChange={handleChangeValue('ratings')}>
           <option value="">Select Ratings</option>
           <option value="1">1 Star</option>
           <option value="2">2 Star</option>
--- src/components/signupForm/index.js
+++ src/components/signupForm/index.js
@@ -1,168 +1,171 @@
 import React, { useEffect, useState } from 'react';
 import './index.css';
 
-class SignupForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstname: '',
-      lastname: '',
-      width: window.innerWidth,
-    };
-    this.handleFirstnameChange = this.handleFirstnameChange.bind(this);
-    this.handleLastnameChange = this.handleLastnameChange.bind(this);
-    this.handleResize = this.handleResize.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener('resize', this.handleResize);
-  }
-
-  componentDidUpdate() {
-    document.title = `${this.state.firstname} ${this.state.lastname}`;
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleResize);
-  }
-
-  handleResize() {
-    this.setState({
-      width: window.innerWidth,
-    });
-  }
-
-  handleFirstnameChange(e) {
-    this.setState({
-      firstname: e.target.value,
-    });
-  }
-
-  handleLastnameChange(e) {
-    this.setState({
-      lastname: e.target.value,
-    });
-  }
-
-  resetForm() {
-    this.setState((state) => {
-      return {
-        ...state,
-        firstname: '',
-        lastname: '',
-      };
-    });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    alert(JSON.stringify(this.state));
-    this.resetForm();
-  }
-
-  render() {
-    return (
-      <form className="form-container" onSubmit={this.handleSubmit}>
-        <div className="field-container">
-          <label htmlFor="firstname">First Name</label>
-          <input
-            id="firstname"
-            name="firstname"
-            type="text"
-            value={this.state.firstname}
-            onChange={this.handleFirstnameChange}
-          />
-        </div>
-        <div className="field-container">
-          <label htmlFor="lastname">Last Name</label>
-          <input
-            id="lastname"
-            name="lastname"
-            type="text"
-            value={this.state.lastname}
-            onChange={this.handleLastnameChange}
-          />
-        </div>
-
-        <div className="field-container">
-          <span>Window Width: {this.state.width}</span>
-        </div>
-        <button className="button" type="submit">
-          Submit
-        </button>
-      </form>
-    );
-  }
-}
-
-// function SignupForm() {
-//   const [firstname, setFisrtname] = useState("");
-//   const [lastname, setLastname] = useState("");
-//   const [width, setWidth] = useState(window.innerWidth);
-
-//   // useEffect(() => {
-//   //   document.title = `${firstname} ${lastname}`;
-//   //   return () => {
-//   //     cleanup
-//   //   }
-//   // }, [input])
-
-//   // useEffect(() => {
-//   //   window.addEventListener("resize", handleResize);
-//   //   return () => {
-//   //     window.removeEventListener("resize", handleResize);
-//   //   }
-//   // }, [])
-
-//   // function handleResize() {
-//   //   setWidth(width);
-//   // }
-
-//   // function resetForm() {
-//   //     return {
-//   //       setFisrtname("");
-//   //       setLastname("");
-//   //     };
-//   // }
-
-//   function handleSubmit(e) {
-//     e.preventDefault();
-//     alert(JSON.stringify([firstname, lastname, width]));
-//     resetForm();
+// class SignupForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       firstname: '',
+//       lastname: '',
+//       width: window.innerWidth,
+//     };
+//     this.handleFirstnameChange = this.handleFirstnameChange.bind(this);
+//     this.handleLastnameChange = this.handleLastnameChange.bind(this);
+//     this.handleResize = this.handleResize.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
 //   }
 
-//   return (
-//     <form className="form-container" onSubmit={handleSubmit}>
-//       <div className="field-container">
-//         <label htmlFor="firstname">First Name</label>
-//         <input
-//           id="firstname"
-//           name="firstname"
-//           type="text"
-//           value={this.state.firstname}
-//           onChange={this.handleFirstnameChange}
-//         />
-//       </div>
-//       <div className="field-container">
-//         <label htmlFor="lastname">Last Name</label>
-//         <input
-//           id="lastname"
-//           name="lastname"
-//           type="text"
-//           value={this.state.lastname}
-//           onChange={this.handleLastnameChange}
-//         />
-//       </div>
+//   componentDidMount() {
+//     window.addEventListener('resize', this.handleResize);
+//   }
 
-//       <div className="field-container">
-//         <span>Window Width: {this.state.width}</span>
-//       </div>
-//       <button className="button" type="submit">
-//         Submit
-//       </button>
-//     </form>
-//   );
+//   componentDidUpdate() {
+//     document.title = `${this.state.firstname} ${this.state.lastname}`;
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('resize', this.handleResize);
+//   }
+
+//   handleResize() {
+//     this.setState({
+//       width: window.innerWidth,
+//     });
+//   }
+
+//   handleFirstnameChange(e) {
+//     this.setState({
+//       firstname: e.target.value,
+//     });
+//   }
+
+//   handleLastnameChange(e) {
+//     this.setState({
+//       lastname: e.target.value,
+//     });
+//   }
+
+//   resetForm() {
+//     this.setState((state) => {
+//       return {
+//         ...state,
+//         firstname: '',
+//         lastname: '',
+//       };
+//     });
+//   }
+
+//   handleSubmit(e) {
+//     e.preventDefault();
+//     alert(JSON.stringify(this.state));
+//     this.resetForm();
+//   }
+
+//   render() {
+//     return (
+//       <form className="form-container" onSubmit={this.handleSubmit}>
+//         <div className="field-container">
+//           <label htmlFor="firstname">First Name</label>
+//           <input
+//             id="firstname"
+//             name="firstname"
+//             type="text"
+//             value={this.state.firstname}
+//             onChange={this.handleFirstnameChange}
+//           />
+//         </div>
+//         <div className="field-container">
+//           <label htmlFor="lastname">Last Name</label>
+//           <input
+//             id="lastname"
+//             name="lastname"
+//             type="text"
+//             value={this.state.lastname}
+//             onChange={this.handleLastnameChange}
+//           />
+//         </div>
+
+//         <div className="field-container">
+//           <span>Window Width: {this.state.width}</span>
+//         </div>
+//         <button className="button" type="submit">
+//           Submit
+//         </button>
+//       </form>
+//     );
+//   }
 // }
+
+function SignupForm() {
+  const [firstname, setFisrtname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [width, setWidth] = useState(window.innerWidth);
+
+  function handleChangeInput(setState) {
+    return (event) => {
+      setState(event.target.value);
+    };
+  }
+
+  function handleResize() {
+    setWidth(window.innerWidth);
+  }
+
+  function resetForm() {
+    setFisrtname('');
+    setLastname('');
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(JSON.stringify([firstname, lastname, width]));
+    resetForm();
+  }
+
+  // componentDidUpdate()
+  useEffect(() => {
+    document.title = `${firstname} ${lastname}`;
+    // [stateIsUpdate] instead componentDidUpdate()
+  }, [firstname, lastname]);
+
+  useEffect(
+    //componentDidMount()
+    () => {
+      window.addEventListener('resize', handleResize);
+
+      // returned function will be called on componentWillUnmount()
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    },
+    []
+  );
+
+  return (
+    <form className="form-container" onSubmit={handleSubmit}>
+      <div className="field-container">
+        <label htmlFor="firstname">First Name</label>
+        <input
+          id="firstname"
+          name="firstname"
+          type="text"
+          value={firstname}
+          onChange={handleChangeInput(setFisrtname)}
+        />
+      </div>
+      <div className="field-container">
+        <label htmlFor="lastname">Last Name</label>
+        <input id="lastname" name="lastname" type="text" value={lastname} onChange={handleChangeInput(setLastname)} />
+      </div>
+
+      <div className="field-container">
+        <span>Window Width: {width}</span>
+      </div>
+      <button className="button" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+}
 
 export default SignupForm;
